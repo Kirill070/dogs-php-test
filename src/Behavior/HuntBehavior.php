1c9b47dd4f8babdc152fac260469f3e1ebaf00f7
Kirill Chernyy <kirill070@gmail.com>
2026-01-21 11:35:06 +0300

    chore: Initial project scaffold

---
+++ src/Behavior/HuntBehavior.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Behavior;
+
+interface HuntBehavior
+{
+    public function hunt(): string;
+}
---
+++ src/Behavior/SoundBehavior.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Behavior;
+
+interface SoundBehavior
+{
+    public function sound(): string;
+}
